885fc1c0ead00b854998884bbbded481b4f9df53
Mark Castillo <markalexandercastillo@gmail.com>
2016-12-20 20:25:41 -0500

    Methods for the two endpoints with response data formatting

--- src/client.js
+++ src/client.js
@@ -1,3 +1,14 @@
+const axios = require('axios')
+  , qs = require('querystring')
+  , wrap = require('word-wrap')
+  , camelcaseKeys = require('camelcase-keys')
+  ;
+
+// extracts the data key and rescursively camelcases keys
+function processResponse({data}) {
+  return camelcaseKeys(data, {deep: true});
+}
+
 const proto = {
   get searchApiUrl() {
     return `${this.siteUrl}/api/search`;
@@ -5,6 +16,19 @@ const proto = {
   get captionApiUrl() {
     return `${this.siteUrl}/api/caption`;
   },
+  getCaptions(screenshot) {
+    const requestUrl = `${this.captionApiUrl}?${qs.stringify({
+      e: screenshot.episode,
+      t: screenshot.timestamp
+    })}`;
+    return axios.get(requestUrl).then(processResponse);
+  },
+  searchScreenshots(terms) {
+    const requestUrl = `${this.searchApiUrl}?${qs.stringify({
+      q: terms
+    })}`;
+    return axios.get(requestUrl).then(processResponse);
+  }
 };
 
 function create(siteUrl) {
